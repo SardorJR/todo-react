ae43cd89186fc84f3d1a40420d71c2f7828d991f
Sardor <sardorjn2006@gmail.com>
2024-08-23 00:46:42 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,79 @@
-import { useState } from 'react'
+import { useEffect, useReducer, useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Todo from './components/todo'
+import "./App.css";
+import Todo from "./components/todo";
+import axios from "axios";
+const initialState = {
+  todos: []
+};
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_TODOS":
+      return { ...state, todos: action.payload };
+    case "ADD_TODO":
+      return { ...state, todos: [...state.todos, action.payload] };
+    case "REMOVE_TODO":
+      return { ...state, todos: state.todos.filter(todo => todo.id !== action.payload) };
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [users, setUsers] = useState([])
-  function Submit(e) {
-    e.preventDefault()
+  const [state, dispatch] = useReducer(reducer, initialState)
+  useEffect(() => {
+    axios.get("http://localhost:8080/todos").then((res) => {
+      dispatch({ type: "SET_TODOS", payload: res.data });
+    });
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
     let todo = {
-      description: new FormData(e.target).get('description'),
-      id: Math.random(),
-      date: new Date().toLocaleDateString('ru-RU', { day: '2-digit', month: '2-digit', year: 'numeric' })
-    }
-    setUsers([...users, todo])
+      description: new FormData(e.target).get("description"),
+      id: String(Math.random()),
+      date: new Date().toLocaleDateString("ru-RU", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      }),
+    };
+    axios.post("http://localhost:8080/todos", todo).then((res) => {
+      dispatch({ type: "ADD_TODO", payload: res.data });
+    });
   }
+
   function removeTodo(id) {
-    setUsers(users.filter(todo => todo.id !== id))
+    axios.delete(`http://localhost:8080/todos/${id}`).then(() => {
+      dispatch({ type: "REMOVE_TODO", payload: id });
+    });
   }
+
   return (
     <>
       <div className="wrap">
-        <div className='neon'>Todo List</div>
-        <form onSubmit={Submit} className="inp_box">
-          <input type="text" name='description' />
-          <button className='todo'>
+        <div className="neon">Todo List</div>
+        <form onSubmit={handleSubmit} className="inp_box">
+          <input type="text" name="description" />
+          <button className="todo">
             <span></span>
             <span></span>
             <span></span>
             <span></span>
             Add
-            </button>
+          </button>
         </form>
         <div className="flex">
-
-          {users.map(todo => (
+          {state.todos.map((todo) => (
             <Todo key={todo.id} user={todo} removeTodo={removeTodo} />
           ))}
-
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,7 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Todo({ user, removeTodo }) {
+    const [isRemoving, setIsRemoving] = useState(false);
 
+   
+    const handleRemove = () => {
+      setIsRemoving(true);
+     
+      setTimeout(() => {
+        removeTodo(user.id);
+      }, 500); 
+    };
     useEffect(() => {
         document.querySelectorAll('h3').forEach(link => {
           link.onclick=(event)=>{
@@ -12,11 +21,11 @@ function Todo({ user, removeTodo }) {
       }, [])
       
     return (
-        <div className="box">
+        <div className={`box ${isRemoving ? 'fade-out' : ''}`}>
             <span></span>
-            <div className="title-box">
+            <div  className="title-box" >
                 <h3>{user.description}</h3>
-                <img onClick={() => removeTodo(user.id)} src="/img/9024966_x_circle_light_icon (1).png" alt="" />
+                <img onClick={handleRemove} src="/img/9024966_x_circle_light_icon (1).png" alt="" />
             </div>
             <div className="date">
                 <p>{user.date}</p>
